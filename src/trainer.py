f2092e28d4786828e8044428aeda7ec63b218d35
Muhammad-Abdelsattar <91436845+Muhammad-Abdelsattar@users.noreply.github.com>
2023-07-03 01:34:54 +0300

    Update trainer.py

--- src/trainer.py
+++ src/trainer.py
@@ -4,5 +4,6 @@ from lightning import Trainer
 
 def get_trainer(config: DictConfig):
     callbacks = list(hydra.utils.instantiate(config.training.callbacks).values())
+    logger = hydra.utils.instantiate(config.training.logger)
     trainer = Trainer(**config.training.trainer,callbacks=callbacks)
     return trainer
